eff06a17e413953e4723e4bd01ccce25e6d06391
Michel Almeida <michelalmeidasilva@outlook.pt>
2021-10-24 22:44:32 -0300

    added nav bar and initial pages

---
+++ src/commons/components/NavBar/NavBar.tsx
@@ -0,0 +1,125 @@
+import React from "react";
+import styled from "styled-components";
+import { NavLink } from "./NavLink";
+import Link, { LinkProps } from "next/link";
+import { FaBars } from "react-icons/fa";
+
+const navLinks = [
+  {
+    name: "Home",
+    path: "/"
+  },
+  {
+    name: "About Us",
+    path: "/about"
+  },
+  {
+    name: "Contact Us",
+    path: "#contact"
+  }
+];
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLogo href='/'>Logo</NavLogo>
+
+        <NavMenu>
+          {navLinks.map((link, index) => (
+            <NavLinkTest key={index} href={link.path}>
+              {link.name}
+            </NavLinkTest>
+          ))}
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+
+export const Nav = styled.nav`
+  background: orangered;
+  height: 85px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.2rem calc((100vw - 1000px) / 2);
+  z-index: 12;
+`;
+
+export const NavLogo = styled(Link)`
+  cursor: pointer;
+  color: #fff;
+  font-size: 2rem;
+  text-decoration: none;
+`;
+
+export const NavLinkTest = styled(Link)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  &.active {
+    color: black;
+  }
+  &:hover {
+    color: black;
+  }
+`;
+
+export const Bars = styled(FaBars)`
+  display: none;
+  color: #fff;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`;
+
+export const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const NavBtnLink = styled(Link)`
+  border-radius: 4px;
+  background: transparent;
+  padding: 10px 22px;
+  color: #fff;
+  outline: none;
+  border: 1px solid #fff;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  margin-left: 24px;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #808080;
+  }
+`;
+
+export default Navbar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from "next/app";
+import { Navbar } from "../commons/components";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 export default MyApp;
